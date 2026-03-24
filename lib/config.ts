88674cfb5dba11367c91d2f7a049d5ef8673eae2
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2026-03-24 15:57:47 +0100

    Refactor API URL configuration to use static values instead of environment variables for API and admin API URLs, enhancing simplicity and consistency.

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,3 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "https://web-server-4gpe.onrender.com";
+export const API_URL = "https://web-server-4gpe.onrender.com";
 
-export const ADMIN_API_URL =
-  process.env.NEXT_PUBLIC_ADMIN_API_URL ??
-  "https://admin-server-syol.onrender.com";
+export const ADMIN_API_URL = "https://admin-server-syol.onrender.com";
