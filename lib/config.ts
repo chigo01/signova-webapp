01b15c3513a55f296c73d66da4ebeb960c2e318d
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2026-04-30 12:47:07 +0100

    update

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = "https://web-server-4gpe.onrender.com";
-export const API_URL = "http://localhost:3003";
+// export const API_URL = "http://localhost:3003";
 
 export const ADMIN_API_URL = "https://admin-server-syol.onrender.com";
 // export const ADMIN_API_URL = "http://localhost:3002";
